453f2c4f599c36920213ce8d4a7ee92e889683c1
angiemichellee <amichelle06@student.ciputra.ac.id>
2026-09-21 21:35:01 +0700

    edit addMenu form

--- viewAddMenu.php
+++ viewAddMenu.php
@@ -10,43 +10,24 @@
     <div class="container p-3">
         <h1>Menambah Menu Baru</h1>
         <form>
+            <div class="form-group">
+                <label for="inputNama">Nama</label>
+                <input type="text" class="form-control" id="inputNama" placeholder="1234 Main St">
+            </div>
+
             <div class="form-row">
                 <div class="form-group col-md-6">
-                <label for="inputNama">Nama</label>
-                <input type="password" class="form-control" id="inputNama" placeholder="Password">
+                <label for="inputKategori">Kategori</label>
+                <input type="password" class="form-control" id="inpinputKategoriutNama" placeholder="Password">
                 </div>
 
                 <div class="form-group col-md-6">
-                <label for="inputKategori">Kategori</label>
-                <input type="email" class="form-control" id="inputKategori" placeholder="Email">
+                <label for="inputHarga">Harga</label>
+                <input type="email" class="form-control" id="inputHarga" placeholder="Email">
                 </div>
                 
             </div>
-            <div class="form-group">
-                <label for="inputAddress">Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-            </div>
-            <div class="form-group">
-                <label for="inputAddress2">Address 2</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                <label for="inputCity">City</label>
-                <input type="text" class="form-control" id="inputCity">
-                </div>
-                <div class="form-group col-md-4">
-                <label for="inputState">State</label>
-                <select id="inputState" class="form-control">
-                    <option selected>Choose...</option>
-                    <option>...</option>
-                </select>
-                </div>
-                <div class="form-group col-md-2">
-                <label for="inputZip">Zip</label>
-                <input type="text" class="form-control" id="inputZip">
-                </div>
-            </div>
+            
             <div class="form-group">
                 <div class="form-check">
                 <input class="form-check-input" type="checkbox" id="gridCheck">

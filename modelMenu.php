93e607b751cf59a68d302055c002fc14a894be55
angiemichellee <amichelle06@student.ciputra.ac.id>
2026-09-21 21:17:13 +0700

    awalan

---
+++ modelMenu.php
@@ -0,0 +1,9 @@
+<?php
+
+class member{
+    public $nama
+    public $kategori
+    public $harga
+}
+
+?>
--- viewMenu.php
+++ viewMenu.php
@@ -3,9 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Menu</title>
 </head>
 <body>
-    <h1>Yooooo</h1>
+    <div class="container p-3">
+        <h1>Yooooo</h1>
+        <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Nama Menu</th>
+      <th scope="col">kategori</th>
+      <th scope="col">Harga</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      <td>
+        <button class="btn btn-warning">Update</button>
+        <button class="btn btn-danger">Delate</button>
+    </tr>
+  </tbody>
+</table>
+    </div>
 </body>
 </html>
